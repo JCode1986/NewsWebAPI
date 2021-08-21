cc3257ed4bf304b16a50e840024c2440461d171b
Joseph Hangarter <joseph.hangarter@yahoo.com>
2021-08-20 20:03:01 -0700

    added top header component

---
+++ src/app/topheading/topheading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topheading',
+  templateUrl: './topheading.component.html',
+  styleUrls: ['./topheading.component.scss']
+})
+export class TopheadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
